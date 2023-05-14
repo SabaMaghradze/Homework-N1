5d0d46d0fd9bf8a486846e278327b8f265d48c68
SabaMaghradze <sabamaghradze1001@gmail.com>
2023-05-14 19:42:54 +0400

    initial commit

---
+++ script.js
@@ -0,0 +1,35 @@
+// problem 1
+
+let name = "saba";
+let surname = "maghradze";
+let gender = "male";
+let yearOfBirth = 2001;
+
+console.log(name + ", " + surname + ", " + gender + ", " + yearOfBirth + ".");
+
+// const personalData = {
+//     name: "saba",
+//     surname: "maghradze",
+//     gender: "male",
+//     yearOfBirth: 2001
+// };
+
+//     console.log(personalData.name)
+
+
+
+// problem 2
+
+//(100 - (10+15)/5)*3
+
+// (10+15) - first expression;
+// (10+15)/5 - second expression;
+// (100 - (10+15)/5) - third expression;
+// (100 - (10+15)/5)*3 - fourth(main) expression;
+
+
+// problem 3
+
+let remainder = 101 % 2;
+
+console.log(remainder)
